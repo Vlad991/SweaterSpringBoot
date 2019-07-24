43d6d23d414beb1e11abadef0bc969f78567afdd
vlad <vladkuzma99@gmail.com>
2019-07-24 22:50:44 +0300

    Add Spring Security

---
+++ src/main/java/com/example/sweater/domain/User.java
@@ -0,0 +1,27 @@
+package com.example.sweater.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private boolean active;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
